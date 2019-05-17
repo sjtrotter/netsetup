f619c75130a2cdd810d0dc6d3cfda39112c2c4fa
sjtrotter <stephen.j.trotter@gmail.com>
2019-05-16 22:07:46 -0500

    fixed indentation issues and added unit test for die when invoked without os defined.

--- netsetup_oswrapper.py
+++ netsetup_oswrapper.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 """netsetup_oswrapper.py
 =========================
-This module is a wrapper class that
-implements os-specific commands for
-use by the netsetup script.
+This module is a wrapper class that implements os-specific commands
+for use by the netsetup script.
 """
+import sys
 
 class OSWrapper():
-  """OS wrapper class. Binds commands
-  into functions based on the OS
-  provided to __init__.
+  """OS wrapper class. Binds commands into functions based on the
+  OS provided to __init__.
   ====
   Required Arguments:
    - os - specify OS to use
   ====
-  Returns: object access to functions
-  that define specific OS commands.
+  Returns: object access to functions that define specific OS
+  commands.
   ====
   Functions:
-    Function definitions are mostly
-    self-explanatory. They are also
+    Function definitions are mostly self-explanatory. They are also
     documented within themselves.
 
    - get_hostname
@@ -64,11 +62,10 @@ class OSWrapper():
    - set_fw_profile
   """
   def __init__(self, os=None):
-  """__init__ method, initializes class. Returns error if os is
-  not defined; sets self.os if it is.  Also sets directory
-  variables based on OS.
-  """
-
+    """__init__ method, initializes class. Returns error if os is
+    not defined; sets self.os if it is.  Also sets directory
+    variables based on OS.
+    """
     self.os = os
     
     if ( self.os == None ):
@@ -77,33 +74,34 @@ class OSWrapper():
     #possibly... add directory definitions here, based on OS.
 
   def _die(self, message=""):
-    print("{}: cannot stat: {}".format(__file__, message), file=sys.stderr)
+    print("{}: cannot stat: {}".format(__file__, \
+      message), file=sys.stderr)
     exit()
     
   def get_hostname(self):
-  """Returns the current hostname.
-  Commands:
-  - Windows - hostname
-  - Linux   - hostname
-  """
+    """Returns the current hostname.
+    Commands:
+    - Windows - hostname
+    - Linux   - hostname
+    """
     #code here
     pass
 
   def set_hostname(self, hostname="mip"):
-  """Sets the hostname.
-  Commands:
-  - Windows - wmic computersystem where name=OLDNAME call rename name={}
-  - Linux   - hostnamectl set-hostname {}
-  ====
-  Arguments:
-  - hostname - the hostname to set
-  ====
-  Returns:
-  - 0 - successful
-  - 1 - unsuccessful
-  ====
-  Windows: sets restart flag
-  """
+    """Sets the hostname.
+    Commands:
+    - Windows - wmic computersystem where name=OLDNAME call rename name={}
+    - Linux   - hostnamectl set-hostname {}
+    ====
+    Arguments:
+    - hostname - the hostname to set
+    ====
+    Returns:
+    - 0 - successful
+    - 1 - unsuccessful
+    ====
+    Windows: sets restart flag
+    """
     # set default hostname based on OS
     if ( hostname =="mip" ):
       if   ( self.os == "Windows" ):
@@ -115,207 +113,207 @@ class OSWrapper():
     pass
 
   def get_hwaddress(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_hwaddress(self, hwaddress=""):
-  """Sets the hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  ====
-  Arguments:
-  - hwaddress - the MAC address to set
-  ====
-  Returns:
-  - 0 - successful
-  - 1 - unsuccessful
-  """
+    """Sets the hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    ====
+    Arguments:
+    - hwaddress - the MAC address to set
+    ====
+    Returns:
+    - 0 - successful
+    - 1 - unsuccessful
+    """
     #code here
     pass
   
   def get_ipaddress(self):
-  """Returns current IP address(es).
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current IP address(es).
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def add_ipaddress(self, ipaddress=""):
-  """Adds IP address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Adds IP address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def del_ipaddress(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def get_gateway(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_gateway(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def del_gateway(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def get_netmask(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_netmask(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def get_dns(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def add_dns(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def del_dns(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def get_ntp(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_ntp(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_trusted(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_targets(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_exclude(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def get_fw_rule(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_fw_rule(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def del_fw_rule(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
   
   def set_fw_profile(self):
-  """Returns current hardware address.
-  Commands:
-  - Windows - 
-  - Linux   - 
-  """
+    """Returns current hardware address.
+    Commands:
+    - Windows - 
+    - Linux   - 
+    """
     #code here
     pass
    
@@ -324,7 +322,9 @@ def main():
   Will input unit tests here.
   """
   #code here
-  pass
+
+  # Unit Test: die if no OS providex
+  netset = OSWrapper()
 
 if ( __name__ == "__main__" ):
   main()
